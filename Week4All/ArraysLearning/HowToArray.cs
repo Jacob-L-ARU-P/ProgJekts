84c979373d203eb1ae0245f55077d089b9be8a22
Jacob L <jl1143@student.aru.ac.uk>
2022-10-13 11:07:17 +0100

    newbranch

--- Week4All/ArraysLearning/HowToArray.cs
+++ Week4All/ArraysLearning/HowToArray.cs
@@ -7,24 +7,12 @@
 String spcr = "    ";
 int[] rainfallamt = new int[12];
 
-rainfallamt[0] = 84;
-rainfallamt[1] = 129;
-rainfallamt[2] = 52;
-rainfallamt[3] = 50;
-rainfallamt[4] = 145;
-rainfallamt[5] = 116;
-rainfallamt[6] = 97;
-rainfallamt[7] = 118;
-rainfallamt[8] = 113;
-rainfallamt[9] = 97;
-rainfallamt[10] = 83;
-rainfallamt[11] = 140;
-
 Console.WriteLine(spcr + "Rainfall Records and Average Calculator: ");
 
-for(int i = 0; i < 12; i++)
+for(int i = 0; i < rainfallamt.Length; i++)
 {
-    Console.WriteLine(spcr + "Rainfall in month " + (i + 1) + " : " + rainfallamt[i]);
+    Console.Write(spcr + "Enter Rainfall for Month " + (i+1) + ": ");
+    rainfallamt[i] = Convert.ToInt32(Console.ReadLine());
 }
 
 Console.WriteLine(spcr + "Rainfall average for the year: " + rainfallamt.Average());
