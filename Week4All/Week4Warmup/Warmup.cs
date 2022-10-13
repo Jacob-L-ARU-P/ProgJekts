3cc2704f6ef0c3aaaf1ea13151b744bb231d416a
Jacob L <jl1143@student.aru.ac.uk>
2022-10-13 13:07:00 +0100

    Used Warmup for Rainfall avg with 2 arrays

--- Week4All/Week4Warmup/Warmup.cs
+++ Week4All/Week4Warmup/Warmup.cs
@@ -1,5 +1,18 @@
 // Hello World
-// 
+// Pain
 
-Console.WriteLine("Rainfall Average Calculator A900!");
-Console.Write("Please enter 12 rainfall values in mm: ");
+Console.WriteLine("Yearly Rainfall Average Calculator:");
+Console.WriteLine("Enter only whole number values, in mm.");
+
+int[] Rainfallmm = new int[12];
+string[] Months = new string[] {"January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+
+for(int i = 0; i < Rainfallmm.Length; i++)
+{
+    Console.Write("Enter Rainfall for " + Months[i] + ": ");
+    Rainfallmm[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine();
+Console.WriteLine("aaa");
+Console.ReadKey();
