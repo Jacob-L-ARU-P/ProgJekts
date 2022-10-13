b22bff9c41032518ae8375f0544ae52e83f6dae0
Jacob L <jl1143@student.aru.ac.uk>
2022-10-13 14:42:23 +0100

    #fixed

--- Week4All/Exercise3/Program.cs
+++ Week4All/Exercise3/Program.cs
@@ -1,19 +1,34 @@
-﻿int[] arr = {800, 11, 50, 771, 649, 770, 240, 9};
-
+﻿// Program Initialisation
+int[] arr = {800, 11, 50, 771, 649, 770, 240, 9};
 int temp = 0;
-
-for (int write = 0; write < arr.Length; write++)
+Console.WriteLine("Bubble Sorter");
+Console.Write("Unsorted Array: ");
+// Initial Array
+for (int x = 0; x < arr.Length; x++)
 {
-    for (int sort = 0; sort < arr.Length - 1; sort++)
-    {
-        if (arr[sort] > arr[sort + 1])
-        {
-            temp = arr[sort + 1];
-            arr[sort + 1] = arr[sort];
-            arr[sort] = temp;
-        }
-    }   
-    Console.Write("{0} ", arr[write]);  
+    Console.Write("{0} ", arr[x]);
 }
-
+// Bubble Sort
+    for (int write = 0; write < arr.Length; write++)
+    {
+        for (int sort = 0; sort < arr.Length - 1; sort++)
+        {
+            if (arr[sort] > arr[sort + 1])
+            {
+                temp = arr[sort + 1];
+                arr[sort + 1] = arr[sort];
+                arr[sort] = temp;
+            }
+        }     
+    }
+// Sorted Array
+Console.WriteLine();
+Console.WriteLine("One Bubble Sort Later...");
+Console.Write("Sorted Array:   ");
+for (int x = 0; x < arr.Length; x++)
+{
+    Console.Write("{0} ", arr[x]);
+}
+// New Line & Window Hold
+Console.WriteLine();
 Console.ReadKey();
